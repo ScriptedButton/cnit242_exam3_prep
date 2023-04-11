7d1b232017690ec271a07bf8c1017db702132846
Cole Brooks <macoryx@gmail.com>
2023-04-11 16:10:23 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Group, Image, Stack} from "@mantine/core";
+import Draggable from 'react-draggable';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showKey, setShowKey] = React.useState(false);
+
+    const images = {
+        components: [
+            "images/application.png",
+            "images/client_spooler.png",
+            "images/gdi.png",
+            "images/gdi_small.png",
+            "images/print_driver.png",
+            "images/print_driver_small.png",
+            "images/print_monitor.png",
+            "images/print_router.png",
+            "images/print_spooler.png"
+        ]
+    };
+
+
+    return (
+        <div className="App">
+            <Stack align={"center"} m={10}>
+                <Image sx={{pointerEvents: "none"}} src={"images/diagram_blank.png"} fit="contain" alt={"diagram_blank"}
+                       height={671} width={1038}/>
+                <Image sx={{pointerEvents: "none", position: "absolute", display: (showKey ? undefined : "none")}} src={"images/answer_key.png"} fit="contain" alt={"diagram_key"} height={671} width={1038}/>
+                <Group>
+                    {images.components.map((image, index) => {
+                        return <Draggable>
+                        <span>
+                                                    <Image sx={{pointerEvents: "none", display: (!showKey ? undefined : "none")}} fit="contain" src={image}
+                                                           alt={`component-${index}`} height={60} width={200}/>
+                        </span>
+                        </Draggable>
+                    })}
+                </Group>
+                <Button onClick={() => setShowKey(!showKey)}>
+                    Reveal Key
+                </Button>
+            </Stack>
+        </div>
+    );
 }
 
 export default App;
